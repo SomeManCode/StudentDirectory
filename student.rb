5ae18e2c6c0e3413161978685f309931ea828356
Joaquin Gamboa <gamboanj@gmail.com>
2013-03-28 22:33:13 -0400

    Refractored initialize and get info method

--- student.rb
+++ student.rb
@@ -1,20 +1,60 @@
 require 'rubygems'
 require 'yaml'
 
+
 class Person
   attr_accessor :name
   attr_accessor :email
   attr_accessor :github_user
   attr_accessor :twitter
   attr_accessor :fun_fact
+
+  #creates an instance of person type
+  def create_person(type)
+    case type
+    when "Student"
+      Student.new
+    when "Instructor"
+      Instructor.new
+    else
+      nil
+    end
+  end
+
+  def initialize
+    print "What is your name? "
+    self.name = gets.strip.chomp
+    print "What is your email? "
+    self.email = gets.strip.chomp
+    print "What is your git hub account? "
+    self.email = gets.strip.chomp
+    print "What is your twitter account? "
+    self.twitter = gets.strip.chomp
+    print "Whats a fun fact about yourself? "
+    self.fun_fact = gets.strip.chomp
+  end
+
 end
+
 
 class Student < Person
   attr_accessor :reason_for_joining
+
+  def initialize
+    #First executes method in super class
+    print "What was your reasoning for joining WDI "
+    self.reason_for_joining = gets.strip.chomp
+  end
 end
 
 class Instructor < Person
   attr_accessor :type
+
+  def initialize
+    #First executes method in super class
+    print "What type of instructor "
+    self.type = gets.strip.chomp
+  end
 end
 
 @directory = ""
@@ -23,26 +63,9 @@ print "Enter Student or Instructor, q to save and quit: "
 
 while ((input = gets.strip.chomp) != 'q') do
 
-  person = nil
-  case input
-  when 'Student' 
-    person = Student.new
-    print "What is your name? "
-    person.name = gets.strip.chomp
-    print "What is your email? "
-    person.email = gets.strip.chomp
-    
-  when 'Instructor'
-    person = Instructor.new
-    print "What is your name? "
-    person.name = gets.strip.chomp
-    print "What is your email? "
-    person.email = gets.strip.chomp
-    print "What sort of instructor are you? "
-    person.type = gets.strip.chomp
-  end
-  
-  # Append this to our yaml file
+  #create instance of the person class to be added in t
+  person = Person.new.create_person(input)
+
   @directory += person.to_yaml
   puts @directory
   
